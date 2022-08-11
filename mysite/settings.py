0112ba70d3c435ff41bef9dcc7b32c7e143b559a
winston mhango <71964085+winstonmhango23@users.noreply.github.com>
2022-08-11 03:21:17 +0200

    changed database settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -94,16 +94,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #     }
 # }
 
-DATABASES = {     
-'default': {
- 'ENGINE': 'django.db.backends.postgresql_psycopg2',
- 'NAME': 'django_blog',
- 'USER': 'postgres',
- 'PASSWORD': 'admin',
- 'HOST': 'localhost',
- 'PORT': ''  
-} 
-} 
+# DATABASES = {     
+# 'default': {
+#  'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#  'NAME': 'django_blog',
+#  'USER': 'postgres',
+#  'PASSWORD': 'admin',
+#  'HOST': 'localhost',
+#  'PORT': ''  
+# } 
+# } 
 
 
 
